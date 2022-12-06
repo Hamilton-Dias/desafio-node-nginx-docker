045263b4aeba2345e4d997d1e9886fe028a8b9fe
Hamilton-Dias <hamiltondias1997@hotmail.com>
2022-12-05 21:09:48 -0300

    inserção e busca banco de dados

---
+++ node/db.js
@@ -0,0 +1,63 @@
+const mysql = require('mysql');
+
+var obj = {
+    con: null,
+
+    connect: () => {
+        if (obj.con)
+            return;
+        
+        obj.con = mysql.createConnection({
+            host: 'db',
+            user: 'root',
+            password: 'root',
+            database: 'nodedb'
+        });
+
+        obj.con.connect(function(err) {
+            if (err) throw err;
+            console.log("Connected!");
+        });
+    },
+
+    end: () => {
+        if (!obj.con)
+            return;
+
+        obj.con.end();
+        obj.con = null;
+    },
+
+    insert: (name) => {
+        obj.connect();
+
+        let sql = "INSERT INTO people (name) VALUES (?)";
+
+        return new Promise((resolve, reject) => {
+            obj.con.query(sql, [name], (error, results, fields) => {
+                if(error) return reject(error);
+                console.log('adicionou nome!');
+                resolve();
+            });
+        });
+    },
+
+    getAll: () => {
+        obj.connect();
+
+        let sql = "SELECT * FROM people";
+
+        return new Promise((resolve, reject) => {
+            obj.con.query(sql, (error, results, fields) => {
+                console.log('buscou nomes...');
+                if(error) 
+                    return reject(error);
+                
+                return resolve(results);
+            });
+        });
+    }
+
+};
+
+module.exports = obj;
--- node/index.js
+++ node/index.js
@@ -1,11 +1,37 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const naampje = require('naampje');
 
-app.get('/', (req, res) => {
-  res.send('<h1>Full Cycle Rocks!</h1>')
+const dbConnection = require('./db.js');
+
+const express = require('express');
+const app = express();
+const port = 8080;
+
+dbConnection.connect();
+
+app.get('/', async (req, res) => {
+
+  await dbConnection.insert(naampje.name());
+
+  const names = await dbConnection.getAll();
+
+  dbConnection.end();
+
+  let html = '<h1>Full Cycle Rocks! </h1>';
+
+  if (!!names && names.length > 0) {
+
+    html += "<ul>";
+
+    names.forEach(item => {
+      html += "<li>" + item.name  + "</li>";
+    });
+
+    html += "</ul>";
+  }
+
+  res.send(html);
 });
 
 app.listen(port, () => {
-  console.log(`App ouvindo na porta ${port}`)
+  console.log(`App ouvindo na porta ${port}`);
 });
